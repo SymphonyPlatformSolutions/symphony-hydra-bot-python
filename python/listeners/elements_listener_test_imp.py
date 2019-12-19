a944beed62a14979e9f2a81c61793435df74fd5d
Reed Feldman <reed.feldman@symphony.com>
2019-12-19 14:29:17 -0500

    removed dashes from env var private key in circle ci

--- python/listeners/elements_listener_test_imp.py
+++ python/listeners/elements_listener_test_imp.py
@@ -3,9 +3,6 @@ from sym_api_client_python.listeners.elements_listener import ElementsActionList
 from sym_api_client_python.processors.sym_elements_parser import SymElementsParser
 from processors.action_processor import ActionProcessor
 from messages.messages import Messages
-# A sample implementation of Abstract imListener class
-# The listener can respond to incoming events if the respective event
-# handler has been implemented
 
 
 class ElementsListenerTestImp(ElementsActionListener):
@@ -17,7 +14,7 @@ class ElementsListenerTestImp(ElementsActionListener):
         self.bot_client = sym_bot_client
         self.bot_id = self.bot_client.bot_id
         self.action_processor = ActionProcessor(self.bot_client)
-        
+
     def on_elements_action(self, action):
         stream_type = self.bot_client.get_stream_client().stream_info_v2(SymElementsParser().get_stream_id(action))
         if stream_type['streamType']['type'] == 'IM':
